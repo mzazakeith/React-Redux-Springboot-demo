d8bf4ff64a203573147ee9a48ea05f1d7c1f1dbf
mzazakeith <mzazakeith@gmail.com>
2022-01-20 20:03:01 +0300

    routing added

--- employeemgtfrontend/src/App.js
+++ employeemgtfrontend/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from './components/Header';
+import EmployeeListing from './components/EmployeeListing'
+import EmployeeDetails from './components/EmployeeDetails'
 
 function App() {
   return (
     <div className="App">
-
+      <div className='App'>
+        <Router>
+          <Header/>
+          <Switch>
+            <Route path="/" exact component={EmployeeListing}/>
+            <Route path="/product/:productId" exact component={EmployeeDetails}/>
+            <Route>404 Not Found!</Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
