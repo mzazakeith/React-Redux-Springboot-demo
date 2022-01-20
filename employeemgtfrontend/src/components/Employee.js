44940b27e5e1f405562d4246f061fe001b0dd63e
mzazakeith <mzazakeith@gmail.com>
2022-01-20 20:44:42 +0300

    able to fetch employees from backend

--- employeemgtfrontend/src/components/Employee.js
+++ employeemgtfrontend/src/components/Employee.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
 
 const Employee = () =>{
+    const employees = useSelector((state) => state.allEmployees.employees);
+    console.log(employees);
     return(
         <div className="ui grid container">
             <p>Hello Employee</p>
