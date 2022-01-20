e295d75585cf5dbc392c109df54933e41161da3a
mzazakeith <mzazakeith@gmail.com>
2022-01-20 20:11:03 +0300

    simple employee listing component method added

---
+++ employeemgtfrontend/src/components/EmployeeListing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const EmployeeListing = () =>{
+    return(
+        <div className="ui grid container">
+            <p>Hello Employee Listing</p>
+        </div>
+    );
+};
+
+export default EmployeeListing;
