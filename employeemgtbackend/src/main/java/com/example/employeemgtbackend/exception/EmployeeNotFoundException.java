a9c79a0577316a13d0d9a4eb8ab300699a47fccf
mzazakeith <mzazakeith@gmail.com>
2022-01-20 19:10:33 +0300

    employee not found exception created

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/exception/EmployeeNotFoundException.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/exception/EmployeeNotFoundException.java
@@ -1,4 +1,21 @@
 package com.example.employeemgtbackend.exception;
 
-public class EmployeeNotFoundException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@Getter
+public class EmployeeNotFoundException extends Exception{
+    private static final long serialVersionUID = 1L;
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public EmployeeNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName,  fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
 }
