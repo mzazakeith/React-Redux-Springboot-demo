6c90b4a19cbfde8dcf48955586dff7a0f24c4546
mzazakeith <mzazakeith@gmail.com>
2022-01-20 22:01:25 +0300

    menu color changed

--- employeemgtfrontend/src/components/Header.js
+++ employeemgtfrontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return(
-        <div className="ui fixed menu">
+        <div className="ui grey inverted fixed menu">
             <div className="ui container center">
                 <h2>Simple Employee Management System</h2>
             </div>
