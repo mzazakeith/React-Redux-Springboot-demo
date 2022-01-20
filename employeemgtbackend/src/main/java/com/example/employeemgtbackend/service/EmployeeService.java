d0e07925c96460918ac01184f11cb6a002e7c5f1
mzazakeith <mzazakeith@gmail.com>
2022-01-20 19:18:52 +0300

    get all employees method added

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/service/EmployeeService.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/service/EmployeeService.java
@@ -1,8 +1,21 @@
 package com.example.employeemgtbackend.service;
 
+import com.example.employeemgtbackend.model.Employee;
+import com.example.employeemgtbackend.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmployeeService {
-    
+    private final EmployeeRepository employeeRepository;
+
+    public List<Employee> getEmployees() {
+        log.info("Fetching all users");
+        return employeeRepository.findAll();
+    }
 }
