b03b7473f295fe6a47e42751e95582548db3e608
mzazakeith <mzazakeith@gmail.com>
2022-01-20 13:26:06 +0300

    initial files and database set

---
+++ employeemgtbackend/src/test/java/com/example/employeemgtbackend/EmployeemgtbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemgtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemgtbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
