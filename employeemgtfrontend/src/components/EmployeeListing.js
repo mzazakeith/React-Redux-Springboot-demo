fe73fc475568b671ae9925006d52b54077d17766
mzazakeith <mzazakeith@gmail.com>
2022-01-20 20:27:48 +0300

    employee listing component done

--- employeemgtfrontend/src/components/EmployeeListing.js
+++ employeemgtfrontend/src/components/EmployeeListing.js
@@ -1,10 +1,28 @@
-import React from "react";
-
+import React, {useEffect} from "react";
+import Employee from "./Employee";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import {listEmployees} from '../redux/actions'
 
 const EmployeeListing = () =>{
+    const dispatch = useDispatch();
+
+    const fetchEmployees = async () => {
+        const response =
+            await axios.get('http://localhost:8080/api/employees')
+                .catch((err)=>{
+                    console.log("Err",err);
+                });
+        dispatch(listEmployees(response.data));
+    };
+
+    useEffect(() => {
+        fetchEmployees();
+    },[]);
+
     return(
         <div className="ui grid container">
-            <p>Hello Employee Listing</p>
+            <Employee/>
         </div>
     );
 };
