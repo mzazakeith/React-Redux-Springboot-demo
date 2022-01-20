d9c3ebfc00e1d3bf885b00582842ecb7b58ddf01
mzazakeith <mzazakeith@gmail.com>
2022-01-20 19:22:27 +0300

    get all employees controller method added

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/controller/EmployeeController.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/controller/EmployeeController.java
@@ -1,10 +1,23 @@
 package com.example.employeemgtbackend.controller;
 
+import com.example.employeemgtbackend.model.Employee;
+import com.example.employeemgtbackend.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api")
+@RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeService employeeService;
 
+    @GetMapping("/users")
+    public ResponseEntity<List<Employee>getEmployees(){
+        return ResponseEntity.ok().body(employeeService.getEmployees());
+    }
 }
