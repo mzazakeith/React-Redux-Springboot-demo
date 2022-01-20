5da93f96a4c77a4d202f4ed233c0fb05ae1638c4
mzazakeith <mzazakeith@gmail.com>
2022-01-20 20:08:19 +0300

    routing removed and header added

--- employeemgtfrontend/src/App.js
+++ employeemgtfrontend/src/App.js
@@ -7,16 +7,7 @@ import EmployeeDetails from './components/EmployeeDetails'
 function App() {
   return (
     <div className="App">
-      <div className='App'>
-        <Router>
           <Header/>
-          <Switch>
-            <Route path="/" exact component={EmployeeListing}/>
-            <Route path="/product/:productId" exact component={EmployeeDetails}/>
-            <Route>404 Not Found!</Route>
-          </Switch>
-        </Router>
-      </div>
     </div>
   );
 }
---
+++ employeemgtfrontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Simple Employee Management System</h2>
+            </div>
+            <br/>
+        </div>
+    )
+};
+
+export default Header;
