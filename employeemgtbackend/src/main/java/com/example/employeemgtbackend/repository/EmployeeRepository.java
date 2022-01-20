0f6c5ccb6aae15479650122e8373b25f38ed265f
mzazakeith <mzazakeith@gmail.com>
2022-01-20 19:05:24 +0300

    employee repository extends jpa

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/repository/EmployeeRepository.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/repository/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.example.employeemgtbackend.repository;
 
-public interface EmployeeRepository {
+import com.example.employeemgtbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    
 }
