78783eefdabfd3f243c8eec6f46dfb48807cc27f
mzazakeith <mzazakeith@gmail.com>
2022-01-20 19:06:10 +0300

    repository annotaion

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/repository/EmployeeRepository.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.employeemgtbackend.repository;
 
 import com.example.employeemgtbackend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-    
+
 }
