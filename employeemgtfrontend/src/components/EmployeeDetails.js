d25c948151579a5119a2b397eb5d4445046565ae
mzazakeith <mzazakeith@gmail.com>
2022-01-20 20:11:41 +0300

    simple employee details component method added

---
+++ employeemgtfrontend/src/components/EmployeeDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const EmployeeDetails = () =>{
+    return(
+        <div className="ui grid container">
+            <p>Hello Employee details</p>
+        </div>
+    );
+};
+
+export default EmployeeDetails;
