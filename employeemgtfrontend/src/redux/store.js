1e1fa049638d26e122b8865d6fd02cd835df10e1
mzazakeith <mzazakeith@gmail.com>
2022-01-20 17:03:08 +0300

    created store and configured indexjs

--- employeemgtfrontend/src/redux/store.js
+++ employeemgtfrontend/src/redux/store.js
@@ -1,7 +1,11 @@
-// import thunk from "redux-thunk";
-// import {composeWithDevTools} from "redux-devtools-extension";
-// import {applyMiddleware, createStore} from "redux";
-//
-// const middleware = [thunk];
-//
-// const store = createStore(,{},composeWithDevTools(applyMiddleware(...middleware)));
+import thunk from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {applyMiddleware, createStore} from "redux";
+import reducers from "./reducers/allReducers";
+
+
+const middleware = [thunk];
+
+const store = createStore(reducers,{},composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
