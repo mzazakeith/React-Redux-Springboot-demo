7cd65e640d56a535ec981686a0e13d972a00be7b
mzazakeith <mzazakeith@gmail.com>
2022-01-21 14:23:34 +0300

    set initial states for the inputs

--- employeemgtfrontend/src/components/AddEmployee.js
+++ employeemgtfrontend/src/components/AddEmployee.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useDispatch} from "react-redux";
 import {useHistory} from "react-router-dom";
 
@@ -6,10 +6,17 @@ const AddEmployee = () =>{
     const dispatch = useDispatch();
     const history = useHistory();
 
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[department, setDepartment] = useState("");
+    const[title, setTitle] = useState("");
+    const[description, setDescription] = useState("");
+
+
 
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(AddEmployee())
+        // dispatch(AddEmployee())
         history.push("/");
     };
 
@@ -18,23 +25,23 @@ const AddEmployee = () =>{
             <form className="ui form big" onSubmit={submitHandler}>
                 <div className="field">
                     <label>Full Name</label>
-                    <input type="text" name="name"  placeholder="John Smith"/>
+                    <input type="text" value={name}  onChange={setName} placeholder="John Smith"/>
                 </div>
                 <div className="field">
                     <label>Email</label>
-                    <input type="email" name="email" placeholder="johnsmith@gmail.com"/>
+                    <input type="email" value={email} onChange={setEmail} placeholder="johnsmith@gmail.com"/>
                 </div>
                 <div className="field">
                     <label>Department</label>
-                    <input type="text" name="department"  placeholder="Finance"/>
+                    <input type="text" value={department} onChange={setDepartment} placeholder="Finance"/>
                 </div>
                 <div className="field">
                     <label>Title</label>
-                    <input type="text" name="title" placeholder="Manager"/>
+                    <input type="text" value={title} onChange={setTitle} placeholder="Manager"/>
                 </div>
                 <div className="field">
                     <label>Job Description</label>
-                    <textarea rows="2" name="description" />
+                    <textarea rows="2" value={description} onChange={setDescription} />
                 </div>
                 <button className="ui button" type="submit">Add</button>
             </form>
