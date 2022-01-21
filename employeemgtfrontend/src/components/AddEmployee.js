fbcf07e3ebd6b5cf34e5ae95add21b257a2e33bf
mzazakeith <mzazakeith@gmail.com>
2022-01-21 14:15:03 +0300

    changed the sumbit to form

--- employeemgtfrontend/src/components/AddEmployee.js
+++ employeemgtfrontend/src/components/AddEmployee.js
@@ -1,16 +1,24 @@
 import React from "react";
+import {useDispatch} from "react-redux";
+import {useHistory} from "react-router-dom";
 
 const AddEmployee = () =>{
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+
     const submitHandler = (e) => {
         e.preventDefault();
+        dispatch(AddEmployee())
+        history.push("/");
     };
 
     return(
         <div className="ui container">
-            <form className="ui form big">
+            <form className="ui form big" onSubmit={submitHandler}>
                 <div className="field">
                     <label>Full Name</label>
-                    <input type="text" name="name" placeholder="John Smith"/>
+                    <input type="text" name="name"  placeholder="John Smith"/>
                 </div>
                 <div className="field">
                     <label>Email</label>
@@ -18,7 +26,7 @@ const AddEmployee = () =>{
                 </div>
                 <div className="field">
                     <label>Department</label>
-                    <input type="text" name="department" placeholder="Finance"/>
+                    <input type="text" name="department"  placeholder="Finance"/>
                 </div>
                 <div className="field">
                     <label>Title</label>
@@ -26,9 +34,9 @@ const AddEmployee = () =>{
                 </div>
                 <div className="field">
                     <label>Job Description</label>
-                    <textarea rows="2" name="description"/>
+                    <textarea rows="2" name="description" />
                 </div>
-                <button className="ui button" type="submit" onClick={submitHandler}>Add</button>
+                <button className="ui button" type="submit">Add</button>
             </form>
         </div>
     )
