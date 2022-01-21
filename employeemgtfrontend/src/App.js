075b5ca9607d2c447c7ef7646e573c9bee143ae9
mzazakeith <mzazakeith@gmail.com>
2022-01-21 12:33:41 +0300

    button and routing for adding an employee added

--- employeemgtfrontend/src/App.js
+++ employeemgtfrontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header from './components/Header';
 import EmployeeListing from './components/EmployeeListing'
 import EmployeeDetails from './components/EmployeeDetails'
+import AddEmployee from "./components/AddEmployee";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Switch>
             <Route path="/" exact component={EmployeeListing}/>
             <Route path="/employee/:employeeId" exact component={EmployeeDetails}/>
+            <Route path="/save/employee/" exact component={AddEmployee}/>
             <Route>404 Not Found!</Route>
           </Switch>
         </Router>
--- employeemgtfrontend/src/components/Header.js
+++ employeemgtfrontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return(
         <div className="ui grey inverted fixed menu">
             <div className="ui container center">
                 <h2>Simple Employee Management System</h2>
+            </div>
+            <div className="ui right floated">
+                <Link to={"/save/employee/"}>
+                    <button className="ui positive button">Add Employee</button>
+                </Link>
             </div>
             <br/>
         </div>
