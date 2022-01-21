cd8ecb9d415f3081363dfeb8088d9e1ce19ab569
mzazakeith <mzazakeith@gmail.com>
2022-01-21 12:50:57 +0300

    form created and simple styling done

--- employeemgtfrontend/src/components/AddEmployee.js
+++ employeemgtfrontend/src/components/AddEmployee.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 const AddEmployee = () =>{
-
-}
+    return(
+        <div className="ui container">
+            <form className="ui form big">
+                <div className="field">
+                    <label>Full Name</label>
+                    <input type="text" name="name" placeholder="John Smith"/>
+                </div>
+                <div className="field">
+                    <label>Email</label>
+                    <input type="email" name="email" placeholder="johnsmith@gmail.com"/>
+                </div>
+                <div className="field">
+                    <label>Department</label>
+                    <input type="text" name="last-name" placeholder="Finance"/>
+                </div>
+                <div className="field">
+                    <label>Title</label>
+                    <input type="text" name="last-name" placeholder="Manager"/>
+                </div>
+                <div className="field">
+                    <label>Job Description</label>
+                    <textarea rows="2"/>
+                </div>
+                <button className="ui button" type="submit">Add</button>
+            </form>
+        </div>
+    )
+};
 
 export default AddEmployee;
+
+
+//     "description
