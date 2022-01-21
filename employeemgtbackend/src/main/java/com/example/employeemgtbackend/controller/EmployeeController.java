0873118c649f5bd5e7e79017278a6b288de3a9de
mzazakeith <mzazakeith@gmail.com>
2022-01-21 16:21:45 +0300

    validation annotation added in controllers

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/controller/EmployeeController.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -25,12 +26,12 @@ public class EmployeeController {
         return new ResponseEntity<Employee>(employeeService.getEmployeeById(employeeId), HttpStatus.OK);
     }
     @PostMapping("/employee/save")
-    public ResponseEntity<Employee>saveUser(@RequestBody Employee employee){
+    public ResponseEntity<Employee>saveUser(@Valid @RequestBody Employee employee){
         return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
     }
 
     @PutMapping("/employee/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long employeeId, @RequestBody Employee employee) throws EmployeeNotFoundException {
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") @Valid long employeeId, @RequestBody Employee employee) throws EmployeeNotFoundException {
         return new ResponseEntity<Employee>(employeeService.updateEmployee(employee,employeeId),HttpStatus.ACCEPTED);
     }
 
