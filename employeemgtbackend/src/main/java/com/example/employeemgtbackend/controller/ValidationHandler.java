b8950f7ba9334944a73db7965240070694a4e4f8
mzazakeith <mzazakeith@gmail.com>
2022-01-21 16:28:08 +0300

    validation handling correction

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/controller/ValidationHandler.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/controller/ValidationHandler.java
@@ -1,4 +1,4 @@
-package com.example.employeemgtbackend.exception;
+package com.example.employeemgtbackend.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
