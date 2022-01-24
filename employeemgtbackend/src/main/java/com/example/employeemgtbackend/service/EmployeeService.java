2e0b1246eb1147ee59f536f6e1d53d825fa47759
mzazakeith <mzazakeith@gmail.com>
2022-01-24 11:38:19 +0300

    delete correction

--- employeemgtbackend/src/main/java/com/example/employeemgtbackend/service/EmployeeService.java
+++ employeemgtbackend/src/main/java/com/example/employeemgtbackend/service/EmployeeService.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Column;
+
 import java.util.List;
 
 @Service
