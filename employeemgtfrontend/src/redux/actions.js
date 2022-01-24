2f4b08de49ed224f52e6c2117bf63e407d6b21f1
mzazakeith <mzazakeith@gmail.com>
2022-01-24 11:49:08 +0300

    update action added

--- employeemgtfrontend/src/redux/actions.js
+++ employeemgtfrontend/src/redux/actions.js
@@ -37,3 +37,20 @@ export const addEmployee = (name, email,department,title,description) => async (
             payload:response.data
         })
 }
+
+export const updateEmployee = (Id,name, email, department, title, description) => async (dispatch) => {
+    const config = {
+        headers:{
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        }
+    };
+    const response = await axios.put(`http://localhost:8080/api/employee/${Id}`,{name, email, department, title, description},config)
+        .catch((err)=>{
+            console.log("Err",err);
+        });
+        dispatch({
+            type:ActionTypes.UPDATE_EMPLOYEE,
+            payload:response.data
+        })
+}
